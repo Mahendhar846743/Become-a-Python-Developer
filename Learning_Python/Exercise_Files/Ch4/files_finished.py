124575887396bc8a8aa6ba99fa0501aeed64fa32
Mahendhar Reddy Mandadi <mahendhar846743@gmail.com>
2021-09-09 08:12:19 +0530

    checked file creation, writing, append, read content and readlines.(file_handling_branch)

--- Learning_Python/Exercise_Files/Ch4/files_finished.py
+++ Learning_Python/Exercise_Files/Ch4/files_finished.py
@@ -5,13 +5,15 @@
 def main():  
   # Open a file for writing and create it if it doesn't exist
   f = open("textfile.txt","w+")
-  
-  # Open the file for appending text to the end
-  # f = open("textfile.txt","a+")
 
   # write some lines of data to the file
   for i in range(10):
     f.write("This is line %d\r\n" % (i+1))
+
+  # Open the file for appending text to the end
+  f = open("textfile.txt","a+")
+  for i in range(10):
+    f.write("This is append line %d\r\n" % (i+1))
   
   # close the file when done
   f.close()
@@ -20,12 +22,12 @@ def main():
   f = open("textfile.txt","r")
   if f.mode == 'r': # check to make sure that the file was opened
     # use the read() function to read the entire file
-    # contents = f.read()
-    # print (contents)
+    full_contents = f.read()
+    print (full_contents)
     
     fl = f.readlines() # readlines reads the individual lines into a list
-    for x in fl:
-      print (x)
+    for line in fl:
+      print (line)
     
 if __name__ == "__main__":
   main()
